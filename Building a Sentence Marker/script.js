518ee8b04b62e1c6a46699f9fcb12887d7d1e831
Pheonix <136172383+Aayusha44@users.noreply.github.com>
2026-06-27 21:20:35 +0545

    Create script.js

---
+++ Building a Sentence Marker/script.js
@@ -0,0 +1,21 @@
+let adjective="big";
+let noun="dragon";
+let verb="angry";
+let place="Castle";
+let adjective2="large";
+let noun2="dwarfs";
+
+let firstStory ="Once upon a time, there was a(n) "+adjective + " " + noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("First story: " + firstStory);
+
+adjective="cute";
+noun="hobbit";
+verb="sad";
+place="shire";
+adjective2="small";
+noun2="hobbitfood";
+
+let secondStory  ="Once upon a time, there was a(n) "+adjective + " " + noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("Second story: " + secondStory);
