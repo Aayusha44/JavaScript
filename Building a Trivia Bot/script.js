3a0dd375d9d0c29ab8133249f8694aadc7552a15
Pheonix <136172383+Aayusha44@users.noreply.github.com>
2026-06-27 20:53:15 +0545

    Create trivia bot script with coding facts
    Initial implementation of a trivia bot that shares coding facts.

---
+++ Building a Trivia Bot/script.js
@@ -0,0 +1,27 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="codeFacts";
+let botLocation="Nepal";
+let favoriteLanguage="English";
+console.log("My name is "+botName+" and I live on "+ botLocation+".");
+console.log("My favorite programming language is "+ favoriteLanguage+".");
+let codingFact="Fun fact is the my favorite language is pretty popular that is "+favoriteLanguage+".";
+console.log(codingFact);
+codingFact="I pratice alot of "+favoriteLanguage+".";
+console.log(codingFact);
+codingFact="My favorite "+favoriteLanguage+" song is Duvet"+".";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye!"+" - "+botName+" from "+botLocation+".");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
